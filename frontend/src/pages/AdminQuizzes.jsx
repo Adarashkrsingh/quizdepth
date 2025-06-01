96291782a68b03f34d9acaa4454be4ee51879648
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-06-01 17:25:06 +0530

    Update AdminQuizzes.jsx

--- frontend/src/pages/AdminQuizzes.jsx
+++ frontend/src/pages/AdminQuizzes.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "../App.css";
 import "./AdminQuizzes.css";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = https://quizdepth.onrender.com;
 
 const AdminQuizzes = () => {
     const [quizzes, setQuizzes] = useState([]);
