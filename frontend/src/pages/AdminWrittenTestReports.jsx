ea87ff80be33623ef9c50dc68ab3141a3431c5b0
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-06-01 17:25:22 +0530

    Update AdminWrittenTestReports.jsx

--- frontend/src/pages/AdminWrittenTestReports.jsx
+++ frontend/src/pages/AdminWrittenTestReports.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../App.css";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = https://quizdepth.onrender.com;
 
 const AdminWrittenTestReports = () => {
     const [reports, setReports] = useState([]);
