b81637dca466cf9c6fb15ac1294815bf3c2fc163
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-06-01 17:29:56 +0530

    Update UserQuiz.jsx

--- frontend/src/pages/UserQuiz.jsx
+++ frontend/src/pages/UserQuiz.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL =https://quizdepth.onrender.com;
 
 const UserQuiz = () => {
     const [quizzes, setQuizzes] = useState([]);
