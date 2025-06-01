21f19451f0b9e9a48efb8407840c45f43517ea5e
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-06-01 17:36:05 +0530

    Update UserWrittenTests.jsx

--- frontend/src/pages/UserWrittenTests.jsx
+++ frontend/src/pages/UserWrittenTests.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "../App.css";
 import "./UserWrittenTests.css"; // ✅ Import the new CSS file
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL =https://quizdepth.onrender.com;
 
 const UserWrittenTests = () => {
     const [tests, setTests] = useState([]);
