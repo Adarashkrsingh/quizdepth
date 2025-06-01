8f0d82997cdc74323d7f017e2b47970416692f6e
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-06-01 17:34:55 +0530

    Update UserReportsCheck.jsx

--- frontend/src/pages/UserReportsCheck.jsx
+++ frontend/src/pages/UserReportsCheck.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "../App.css";
 import "./UserReportsCheck.css";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = https://quizdepth.onrender.com;
 
 const UserReportsCheck = () => {
     const { id } = useParams();
